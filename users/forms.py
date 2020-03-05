e6de64b10428db7a5c02f57262f4ee9ecd260b63
Clark L Smith <clarklander1983@gmail.com>
2020-03-04 18:55:54 -0600

    fixed login error messages

--- users/forms.py
+++ users/forms.py
@@ -25,23 +25,44 @@ class AdtaaAuthenticationForm(AuthenticationForm):
     def clean(self):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
+    #
+    #     if username is not None and password:
+    #         user = authenticate(username=username, password=password)
+    #         if user is not None:
+    #             if not user.is_active:
+    #                 raise forms.ValidationError(
+    #                     self.error_messages['inactive'],
+    #                     code='inactive',
+    #                 )
+    #
+    #
+    #
+    #         else:
+    #             raise forms.ValidationError(
+    #                 self.error_messages['invalid_login'],
+    #                 code='invalid_login',
+    #                 params={'username': self.username_field.verbose_name},
+    #             )
+    #
+    #     return self.cleaned_data
 
-        if username is not None and password:
-            self.user_cache = authenticate(self.request, username=username, password=password)
-            if self.user_cache is None:
-                try:
-                    user_temp = AdtaaUser.objects.get(username=username)
-                except:
-                    user_temp = None
 
-                if user_temp is not None:
-                    self.confirm_login_allowed(user_temp)
-                else:
-                    raise forms.ValidationError(
-                        self.error_messages['invalid_login'],
-                        code='invalid_login',
-                        params={'username': self.username_field.verbose_name},
-                    )
+
+        self.user_cache = authenticate(self.request, username=username, password=password)
+        if self.user_cache is not None:
+            try:
+                user_temp = AdtaaUser.objects.get(username=username)
+            except:
+                user_temp = None
+
+            if user_temp is not None:
+                self.confirm_login_allowed(user_temp)
+        else:
+            raise forms.ValidationError(
+                self.error_messages['invalid_login'],
+                code='invalid_login',
+                params={'username': self.username_field.verbose_name},
+            )
 
         return self.cleaned_data
 
