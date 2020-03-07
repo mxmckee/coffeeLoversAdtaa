257bb28abf4a3d1e2e71951ff00c1b13554c82f0
Clark L Smith <clarklander1983@gmail.com>
2020-03-07 02:41:08 -0600

    course and instructor models added, course setup styled.

--- Adtaa/urls.py
+++ Adtaa/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from Adtaa import views as Adtaa_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('courselist/', Adtaa_views.CourseListView.as_view(template_name='Adtaa/courselist.html'), name='courselist'),
+    path('courseadd/', Adtaa_views.CourseAddView.as_view(template_name='Adtaa/course_form.html'), name='course-add'),
 ]
--- Adtaa/views.py
+++ Adtaa/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Course, Instructor
 
 #this is the views page for the adtaa
 
 def index(request):
     return render(request, 'Adtaa/index.html')
+
+class CourseListView(ListView):
+    model = Course
+    context_object_name = 'courses'
+
+class CourseAddView(LoginRequiredMixin, CreateView):
+    model = Course
+    fields = ['courseNumber', 'courseTitle', 'courseDays', 'courseTime', 'instructor', 'discipline1', 'discipline2']
