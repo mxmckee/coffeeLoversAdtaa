52004b34650b092d07c1363a8c4d00f95e400b40
bplovell <54449704+bplovell@users.noreply.github.com>
2020-03-21 12:11:23 -0500

    adding admin function to app
    Does not send emails from app!

--- coffeeLoversAdtaa/urls.py
+++ coffeeLoversAdtaa/urls.py
@@ -20,7 +20,6 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from invitations import views as invitation_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -45,6 +44,9 @@ urlpatterns = [
     path('user/<int:pk>/', user_views.UserDetailView.as_view(template_name='users/AdtaaUser_detail.html'), name='user-detail'),
     path('user/<int:pk>/update/', user_views.UserUpdateView.as_view(template_name='users/user_form.html'), name='user-update'),
     path('accept-invite/', user_views.root_register, name='account_signup'),
+    path('user/rootinvite/', user_views.RootInvite, name='rootinvite'),
+    path('user/rootinvitechange/', user_views.RootInviteChange, name='rootinvitechange'),
+
 
     path('', include('Adtaa.urls')),
 
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import AdtaaUser
+from invitations.views import SendInvite
+from invitations.forms import InvitationAdminAddForm, InvitationAdminChangeForm
 from .forms import AdtaaUserForm, AdtaaAuthenticationForm, AdtaaRootUserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -40,6 +42,31 @@ def root_register(request):
         form = AdtaaRootUserForm()
 
     return render(request, 'users/account_signup.html', {'form':form})
+
+def RootInvite(request):
+    if request.method == 'POST':
+        form=InvitationAdminAddForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            messages.success(request, f'Invite sent!')
+            return redirect('rootinvite')
+    else:
+        form = InvitationAdminAddForm()
+
+    return render(request, 'users/rootinvite.html', {'form':form})
+
+def RootInviteChange(request):
+    if request.method == 'POST':
+        form=InvitationAdminChangeForm(request.POST)
+        if form.is_valid():
+            #user=form.save()
+            #user.save()
+            #messages.success(request, f'Invite sent!')
+            return redirect('profile')
+    else:
+        form = InvitationAdminChangeForm()
+
+    return render(request, 'users/profile.html', {'form':form})
 
 def change_password(request):
     if request.method == 'POST':
